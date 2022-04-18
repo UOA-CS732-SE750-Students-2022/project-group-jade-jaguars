3ffb0e05155ab161ebcc273bf3bc7a1b50b23221
danielle161 <danielle.print@xtra.co.nz>
2022-04-18 19:47:23 +1200

    Creates component based on potential times and availability

--- packages/frontend/components/AvailabilitySelector.tsx
+++ packages/frontend/components/AvailabilitySelector.tsx
@@ -1,6 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
+import TimeBracket from '../types/TimeBracket';
 
-function AvailabilitySelector(props: { numRows: number; numCols: number }) {
+function AvailabilitySelector(props: {
+  potentialTimes: TimeBracket[];
+  availability: TimeBracket[];
+}) {
   const firstRender = useRef(true);
   const [selecting, setSelecting] = useState<Boolean>(false);
   const [start, setStart] = useState<number[]>([-1, -1]);
@@ -21,25 +25,72 @@ function AvailabilitySelector(props: { numRows: number; numCols: number }) {
   >([]);
 
   useEffect(() => {
+    const startTime = new Date(props.potentialTimes[0].startTime);
+    const startTimeHours = startTime.getHours();
+    const startTimeMins = startTime.getMinutes();
+    const endTime = new Date(props.potentialTimes[0].endTime);
+    const endTimeHours = endTime.getHours();
+    const endTimeMins = endTime.getMinutes();
+    const hours = endTimeHours - startTimeHours;
+
+    let halfHours = hours * 2;
+
+    if (startTimeMins == 30) {
+      halfHours--;
+    }
+    if (endTimeMins == 30) {
+      halfHours--;
+    }
+
     let initialTimeSlots: {
       row: number;
       col: number;
       selected: boolean;
     }[] = [];
 
-    for (let i = 0; i < props.numRows; i++) {
-      for (let j = 0; j < props.numCols; j++) {
+    for (let i = 0; i < halfHours; i++) {
+      for (let j = 0; j < props.potentialTimes.length; j++) {
+        const day = new Date(props.potentialTimes[j].startTime);
+        let time = startTimeHours + 0.5 * i;
+        if (startTimeMins == 30) {
+          time += 0.5;
+        }
+        let hours;
+        let mins;
+        if (time % 1 === 0) {
+          hours = time;
+          mins = 0;
+        } else {
+          hours = time - 0.5;
+          mins = 30;
+        }
+        const datetime = new Date(
+          day.getFullYear(),
+          day.getMonth(),
+          day.getDate(),
+          hours,
+          mins,
+        );
+        let isSelected = false;
+        for (let index in props.availability) {
+          const startDT = new Date(props.availability[index].startTime);
+          const endDT = new Date(props.availability[index].endTime);
+          if (datetime >= startDT && datetime < endDT) {
+            isSelected = true;
+            break;
+          }
+        }
         initialTimeSlots.push({
           row: i,
           col: j,
-          selected: false,
+          selected: isSelected,
         });
       }
     }
 
     setTimeSlots(initialTimeSlots);
     setSelection(initialTimeSlots);
-  }, [props.numRows, props.numCols]);
+  }, [props.potentialTimes, props.availability]);
 
   useEffect(() => {
     if (firstRender.current) {
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -3,9 +3,49 @@ import Head from 'next/head';
 import Image from 'next/image';
 import AvailabilitySelector from '../components/AvailabilitySelector';
 import styles from '../styles/Home.module.css';
+import TimeBracket from '../types/TimeBracket';
+
+const potentialTimes: TimeBracket[] = [
+  {
+    startTime: 1650229200000,
+    endTime: 1650258000000,
+  },
+  {
+    startTime: 1650315600000,
+    endTime: 1650344400000,
+  },
+  {
+    startTime: 1650402000000,
+    endTime: 1650430800000,
+  },
+  {
+    startTime: 1650488400000,
+    endTime: 1650517200000,
+  },
+  {
+    startTime: 1650574800000,
+    endTime: 1650603600000,
+  },
+];
+
+const availability: TimeBracket[] = [
+  {
+    startTime: 1650232800000,
+    endTime: 1650250800000,
+  },
+  {
+    startTime: 1650402000000,
+    endTime: 1650430800000,
+  },
+];
 
 const Home: NextPage = () => {
-  return <AvailabilitySelector numRows={16} numCols={5} />;
+  return (
+    <AvailabilitySelector
+      potentialTimes={potentialTimes}
+      availability={availability}
+    />
+  );
 };
 
 export default Home;
