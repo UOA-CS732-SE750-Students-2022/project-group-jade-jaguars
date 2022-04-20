f46d03e43d86e2179127779d4767c66f795e39f2
Samuel Chen <luffymax0322@gmail.com>
2022-04-20 23:39:11 +1200

    added form implementation

--- packages/frontend/components/EventForm.tsx
+++ packages/frontend/components/EventForm.tsx
@@ -1,34 +1,65 @@
 import React, { useState } from 'react';
 import { DateRangePicker, TimeRangeInput } from '@mantine/dates';
-import { Box, Grid, Paper, Textarea, TextInput } from '@mantine/core';
-import dayjs from 'dayjs';
+import {
+  Box,
+  Button,
+  Grid,
+  Group,
+  Paper,
+  Textarea,
+  TextInput,
+} from '@mantine/core';
+import { useForm } from '@mantine/hooks';
 interface FormValues {
   title: string;
-  dates: Date[];
-  // time:
+  dateRange: [Date | null, Date | null];
+  timeRange: [Date, Date];
+  description?: string;
+  location?: string;
 }
 const EventForm = () => {
-  const [value, setValue] = useState<[Date | null, Date | null]>([
-    new Date(),
-    new Date(),
-  ]);
+  const form = useForm<FormValues>({
+    initialValues: {
+      title: '',
+      dateRange: [new Date(), new Date()],
+      timeRange: [new Date(), new Date()],
+      description: '',
+      location: '',
+    },
+  });
   return (
     <Box sx={{ maxWidth: 700 }} mx="auto">
-      <Paper p="xl" withBorder>
+      <Paper p="xl" radius="md" withBorder>
         <Grid className="m-[50px]">
           <Grid.Col>
-            <TextInput label="Event Title" placeholder="add event title" />
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <DateRangePicker
-              label="Date Range"
-              placeholder="Pick dates range"
-              value={value}
-              onChange={setValue}
+            <TextInput
+              required
+              label="Event Title"
+              placeholder="add event title"
+              value={form.values.title}
+              onChange={(e) =>
+                form.setFieldValue('title', e.currentTarget.value)
+              }
             />
           </Grid.Col>
           <Grid.Col span={6}>
-            <TimeRangeInput label="Time Range" />
+            <DateRangePicker
+              required
+              label="Date Range"
+              placeholder="Pick dates range"
+              value={form.values.dateRange}
+              onChange={(e) => form.setFieldValue('dateRange', [e[0], e[1]])}
+              minDate={new Date()}
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <TimeRangeInput
+              label="Time Range"
+              value={form.values.timeRange}
+              onChange={(e) => form.setFieldValue('timeRange', [e[0], e[1]])}
+              required
+              clearable
+            />
           </Grid.Col>
           <Grid.Col>
             <Textarea
@@ -37,13 +68,33 @@ const EventForm = () => {
               autosize
               minRows={2}
               maxRows={4}
+              value={form.values.description}
+              onChange={(e) =>
+                form.setFieldValue('description', e.currentTarget.value)
+              }
             />
           </Grid.Col>
           <Grid.Col>
             <TextInput
               label="Location"
               placeholder="location or meeting link"
+              value={form.values.location}
+              onChange={(e) =>
+                form.setFieldValue('description', e.currentTarget.value)
+              }
             />
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Group position="right" mt="lg">
+              <Button
+                classNames={{
+                  filled: 'bg-[#FFDF74] hover:bg-[#FFDF74]',
+                  label: 'text-black',
+                }}
+              >
+                Done
+              </Button>
+            </Group>
           </Grid.Col>
         </Grid>
       </Paper>
