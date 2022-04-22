f785686618e21380861452153d434ec096c62e05
danielle161 <danielle.print@xtra.co.nz>
2022-04-22 21:35:14 +1200

    Removed api dependencies in component

--- packages/frontend/components/TeamCard.tsx
+++ packages/frontend/components/TeamCard.tsx
@@ -1,28 +1,29 @@
-import Team from '../types/Team';
+import TeamDetails from '../types/TeamDetails';
+import Member from '../types/Member';
 import { Card, Text, Grid, Avatar } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 
-function TeamCard(props: Team) {
-  const [members, setMembers] = useState<string[]>([]);
+function TeamCard(team: TeamDetails) {
+  const [members, setMembers] = useState<Member[]>([]);
   const [count, setCount] = useState<number>(0);
 
   useEffect(() => {
-    let members = [...props.members];
+    let members = [...team.members];
     let count = members.length - 7;
     if (count < 0) {
       count = 0;
     }
     setCount(count);
     setMembers(members.splice(0, 7));
-  }, [props]);
+  }, [team]);
 
   return (
-    <Card css={{ mw: '290px', height: '180px' }}>
+    <Card css={{ mw: '295px', height: '180px' }}>
       <Text h4 css={{ paddingTop: '20px', marginLeft: '5px' }}>
-        {props.title}
+        {team.title}
       </Text>
       <Text css={{ fontSize: '$tiny', marginLeft: '5px' }}>
-        {props.description}
+        {team.description}
       </Text>
       <Avatar.Group
         count={count}
@@ -30,18 +31,11 @@ function TeamCard(props: Team) {
       >
         {/* Map each member profile pic to an avatar */}
         {members.map((member, index) => {
-          const photoUrl = member;
-          /* let photoUrl = "";
-                            // Get member's profile image:
-                            getAuth()
-                                .getUser(member)
-                                .then((userRecord) => {
-                                    photoUrl = userRecord.getPhotoUrl();
-                                })
-                                .catch((error) => {
-                                    console.log('Error fetching user data:', error);
-                                }); */
-
+          let photoUrl: string =
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+          if (member.profilePic) {
+            photoUrl = member.profilePic;
+          }
           return (
             <Avatar key={index} size="md" src={photoUrl} bordered stacked />
           );
