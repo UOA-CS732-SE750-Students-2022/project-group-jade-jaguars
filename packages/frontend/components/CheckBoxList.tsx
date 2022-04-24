92ae924c953d6aeb6c001c0c80ccd40cf19d799a
Samuel Chen <luffymax0322@gmail.com>
2022-04-25 10:51:04 +1200

    change checkboxlist implementation

--- packages/frontend/components/CheckBoxList.tsx
+++ packages/frontend/components/CheckBoxList.tsx
@@ -1,5 +1,6 @@
 import { Checkbox, ScrollArea } from '@mantine/core';
 import React from 'react';
+import { TeamCheckBox } from './TeamCheckBox';
 
 const CheckBoxList = () => {
   const teamList = [
@@ -12,38 +13,11 @@ const CheckBoxList = () => {
     'Board Game Club',
     'Board Game Club',
   ];
-  const inputBackgroundColorList = [
-    '#BDD8FF',
-    '#FFE074',
-    '#FCDDEC',
-    '#FF9F9F',
-    '#E2B1FF',
-  ];
+
   return (
     <ScrollArea style={{ height: 100, width: 300 }}>
       {teamList.map((team, index) => {
-        const color = 'teal';
-        const inputBackgroundColor = inputBackgroundColorList[index % 5];
-        const inputStyle = `ml-3 border-solid border-black border-2`;
-        return (
-          <Checkbox
-            key={index}
-            color={color}
-            label={team}
-            classNames={{
-              root: 'hover:bg-[#99C08B] w-full h-full rounded-md',
-              label: 'w-full p-2 ml-3 hover:text-white',
-              input: inputStyle,
-              icon: 'ml-[17px] text-black',
-            }}
-            styles={{
-              input: {
-                backgroundColor: inputBackgroundColor,
-                color: inputBackgroundColor,
-              },
-            }}
-          />
-        );
+        <TeamCheckBox label={team} order={index} key={index} />;
       })}
     </ScrollArea>
   );
