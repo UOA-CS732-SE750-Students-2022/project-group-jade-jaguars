32cc18c578098dbdaec56c51d951ede1bfcf500d
Raina Song <rainasong0509@gmail.com>
2022-04-24 22:56:25 +1200

    add custom colours

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      primary: '#99C08B', // primary green
+      primarylight: '#FAF1E6', // light green
+      secondary: '#FFE074', // secondary yellow
+      secondarylight: '#FFF6D7', // light yellow
+      backgroundgrey: '#F9F9F9',
+      cardgrey: '#FAFAFA',
+    },
     extend: {},
   },
   plugins: [],
