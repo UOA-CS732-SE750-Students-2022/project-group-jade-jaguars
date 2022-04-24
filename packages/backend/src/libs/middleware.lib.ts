1a4d9570d6000e9af0c5790e11b45a43f08368ff
Etienne <eti@naude.dev>
2022-04-24 14:49:20 +1200

    authentication error messages

--- packages/backend/src/libs/middleware.lib.ts
+++ packages/backend/src/libs/middleware.lib.ts
@@ -2,13 +2,20 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 import { getAuth } from 'firebase-admin/auth';
 
-export const isAuthenticated = async (
+export async function isAuthenticated(
   req: Request,
   res: Response,
   next: NextFunction,
-) => {
+) {
   try {
+    if (!req.headers.authorization) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: 'Unauthenticated' });
+    }
+
     const token = req.headers.authorization.split(' ')[1];
+
     const decodedValue = await getAuth().verifyIdToken(token);
 
     if (decodedValue) {
@@ -19,8 +26,16 @@ export const isAuthenticated = async (
       .status(StatusCodes.UNAUTHORIZED)
       .json({ message: 'Unauthenticated' });
   } catch (error) {
+    console.log(error);
+
+    if (error.code === 'auth/id-token-expired') {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: 'Token expired' });
+    }
+
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: 'Internal Server Error' });
   }
-};
+}
