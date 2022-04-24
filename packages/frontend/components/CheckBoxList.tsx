b61e14ff52f3d66eced57b889f903003598ab55b
Samuel Chen <luffymax0322@gmail.com>
2022-04-25 11:16:41 +1200

    Revert "refactor CheckBox implementation"
    This reverts commit 3ce96517e6cd136a02bb398df6199db84cee6581.

--- packages/frontend/components/CheckBoxList.tsx
+++ packages/frontend/components/CheckBoxList.tsx
@@ -1,6 +1,6 @@
-import { ScrollArea } from '@mantine/core';
+import { Checkbox, ScrollArea } from '@mantine/core';
+import { useHover } from '@mantine/hooks';
 import React from 'react';
-import { TeamCheckBox } from './TeamCheckBox';
 
 const CheckBoxList = () => {
   const teamList = [
@@ -13,10 +13,43 @@ const CheckBoxList = () => {
     'Board Game Club',
     'Board Game Club',
   ];
+  const inputBackgroundColorList = [
+    '#BDD8FF',
+    '#FFE074',
+    '#FCDDEC',
+    '#FF9F9F',
+    '#E2B1FF',
+  ];
   return (
     <ScrollArea style={{ height: 100, width: 300 }}>
       {teamList.map((team, index) => {
-        return <TeamCheckBox key={index} label={team} order={index} />;
+        const color = 'teal';
+        const inputBackgroundColor = inputBackgroundColorList[index % 5];
+        const inputStyle = `ml-3 border-solid border-black border-2 cursor-pointer`;
+        const { hovered, ref } = useHover();
+        return (
+          <div ref={ref} key={index}>
+            <Checkbox
+              key={index}
+              color={color}
+              label={team}
+              classNames={{
+                root: 'hover:bg-[#99C08B] w-full h-full rounded-md',
+                label: hovered
+                  ? 'w-full p-2 ml-3 text-white cursor-pointer'
+                  : 'w-full p-2 ml-3 text-black cursor-pointer',
+                input: inputStyle,
+                icon: 'ml-[17px] text-black cursor-pointer',
+              }}
+              styles={{
+                input: {
+                  backgroundColor: inputBackgroundColor,
+                  color: inputBackgroundColor,
+                },
+              }}
+            />
+          </div>
+        );
       })}
     </ScrollArea>
   );
