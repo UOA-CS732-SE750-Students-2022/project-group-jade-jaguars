f6e6c568396031210fe7142292d055d7425c3c31
danielle161 <63983821+danielle161@users.noreply.github.com>
2022-04-29 16:08:34 +1200

    Group availability component (#60)
    * Created group availability component

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next';
-import AvailabilitySelector from '../components/AvailabilitySelector';
+import GroupAvailability from '../components/GroupAvailability';
 import TimeBracket from '../types/TimeBracket';
 import {
-  AvailabilityBlock,
+  AttendeeAvailability,
+  AttendeeStatus,
   AvailabilityStatus,
-} from '../types/AvailabilityBlock';
+} from '../types/Availability';
 
 const timeOptions: TimeBracket[] = [
   {
@@ -29,69 +30,54 @@ const timeOptions: TimeBracket[] = [
   },
 ];
 
-const availability: AvailabilityBlock[] = [
+const availabilities: AttendeeAvailability[] = [
   {
-    startTime: 1650232800000,
-    endTime: 1650250800000,
-    status: AvailabilityStatus.Available,
+    uuid: 'Brad',
+    availability: [
+      {
+        startTime: 1650232800000,
+        endTime: 1650250800000,
+        status: AvailabilityStatus.Available,
+      },
+      {
+        startTime: 1650402000000,
+        endTime: 1650430800000,
+        status: AvailabilityStatus.Tentative,
+      },
+    ],
   },
   {
-    startTime: 1650250800000,
-    endTime: 1650258000000,
-    status: AvailabilityStatus.Unavailable,
-  },
-  {
-    startTime: 1650315600000,
-    endTime: 1650344400000,
-    status: AvailabilityStatus.Unavailable,
-  },
-  {
-    startTime: 1650402000000,
-    endTime: 1650430800000,
-    status: AvailabilityStatus.Tentative,
-  },
-  {
-    startTime: 1650488400000,
-    endTime: 1650517200000,
-    status: AvailabilityStatus.Unavailable,
-  },
-  {
-    startTime: 1650574800000,
-    endTime: 1650603600000,
-    status: AvailabilityStatus.Unavailable,
+    uuid: 'Chad',
+    availability: [
+      {
+        startTime: 1650243600000,
+        endTime: 1650254400000,
+        status: AvailabilityStatus.Available,
+      },
+      {
+        startTime: 1650405600000,
+        endTime: 1650423600000,
+        status: AvailabilityStatus.Available,
+      },
+      {
+        startTime: 1650495600000,
+        endTime: 1650517200000,
+        status: AvailabilityStatus.Tentative,
+      },
+    ],
   },
 ];
 
+const handleHover = (info: { people: AttendeeStatus[]; numPeople: number }) => {
+  console.log(info);
+};
+
 const Home: NextPage = () => {
-  const participants = [
-    {
-      name: 'Amy',
-      profilePic:
-        'https://media-exp1.licdn.com/dms/image/C4E0BAQHUo_h0JGtwYw/company-logo_200_200/0/1606490589727?e=2147483647&v=beta&t=TO869IrmjUEr7VSFzSHaqcEN4_-TTctFucuyBv8cqDA',
-    },
-    {
-      name: 'Bob',
-      profilePic:
-        'https://img.pixers.pics/pho_wat(s3:700/FO/23/80/66/66/700_FO23806666_a4cd1ba91572617e8833dcbd1d17a44c.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-the-letter-b.jpg.jpg',
-    },
-    {
-      name: 'Carlie',
-      // profilePic: '',
-    },
-    {
-      name: 'David',
-      // profilePic: '',
-    },
-    {
-      name: 'Eric',
-      // profilePic: '',
-    },
-  ];
   return (
-    <AvailabilitySelector
+    <GroupAvailability
       timeOptions={timeOptions}
-      availability={availability}
-      status={AvailabilityStatus.Available}
+      availabilities={availabilities}
+      onHover={handleHover}
     />
   );
 };
---
+++ packages/frontend/types/Availability.tsx
@@ -0,0 +1,21 @@
+export interface AvailabilityBlock {
+  startTime: number;
+  endTime: number;
+  status: AvailabilityStatus;
+}
+
+export enum AvailabilityStatus {
+  Available,
+  Unavailable,
+  Tentative,
+}
+
+export interface AttendeeAvailability {
+  uuid: string; // uuid of the attendee
+  availability: AvailabilityBlock[];
+}
+
+export interface AttendeeStatus {
+  uuid: string;
+  status: AvailabilityStatus;
+}
