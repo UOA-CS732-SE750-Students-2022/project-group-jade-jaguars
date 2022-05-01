380d2a071834e7cb70c4e2a7dcf98567401d0daa
Samuel Chen <luffymax0322@gmail.com>
2022-05-01 14:17:14 +1200

    styled landing page

--- packages/frontend/components/CustomNavbar.tsx
+++ packages/frontend/components/CustomNavbar.tsx
@@ -5,6 +5,7 @@ import { CalendarEvent, LayoutDashboard, Users } from 'tabler-icons-react';
 import { useAuth } from '../src/context/AuthContext';
 import { NavbarLink } from './NavbarLink';
 import { getAuth } from 'firebase/auth';
+import { useRouter } from 'next/router';
 
 const linkData = [
   { icon: LayoutDashboard, label: 'Dashboard' },
@@ -13,12 +14,13 @@ const linkData = [
 ];
 export const CustomNavbar = () => {
   const { height, width } = useViewportSize();
-  const [active, setActive] = useState(2);
+  const [active, setActive] = useState(0);
   const { user, setUser } = useAuth();
-
+  const router = useRouter();
   const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
     user ? setIsLogin(true) : setIsLogin(false);
+    !user && router.push('/');
   }, [user]);
 
   useEffect(() => {
@@ -33,10 +35,8 @@ export const CustomNavbar = () => {
     <Navbar height={height} width={{ base: 100 }} className="border-1">
       <Navbar.Section>
         <div className="w-[100px]">
-          <Group direction="column" align="center" mt={50}>
-            <Avatar color="teal" radius="lg" size={70}>
-              Logo
-            </Avatar>
+          <Group direction="column" align="center" mt={10}>
+            <img src="/logo.svg" width={50} />
           </Group>
         </div>
       </Navbar.Section>
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Container, Group } from '@mantine/core';
+import { Container, Grid, Group } from '@mantine/core';
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
-import { HeartHandshake } from 'tabler-icons-react';
 import { useAuth } from '../src/context/AuthContext';
 import { useRouter } from 'next/router';
 import { getAuth } from 'firebase/auth';
+import Image from 'next/image';
 const Home: NextPage = () => {
   const { user, login, logout, signedIn, setUser } = useAuth();
   const router = useRouter();
@@ -12,25 +12,43 @@ const Home: NextPage = () => {
     signedIn ? router.push('/demo') : router.push('/');
   }, [signedIn]);
 
-  useEffect(() => {
-    const clearListener = getAuth().onAuthStateChanged((user) => {
-      setUser(user);
-    });
-    return () => {
-      clearListener();
-    };
-  }, [setUser]);
   return (
     <Container>
-      <nav className="border p-5">
-        <Group direction="row">
-          <HeartHandshake />
-          <div className="font-semibold">Count Me In</div>
+      <nav className=" p-5">
+        <Group direction="row" align="center">
+          <Image src="/logo.svg" width={70} height={70} />
+          <h1 className="mb-[-10px]">CountMeIn</h1>
         </Group>
       </nav>
-
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
+      <Grid align="center">
+        <Grid.Col md={12} lg={6}>
+          <span>
+            <Group direction="column" align={'center'}>
+              <Image
+                src="/landing-banner.svg"
+                alt="banner"
+                width={600}
+                height={400}
+              />
+              <a className="text-secondary" href="https://storyset.com/">
+                Illustrations by Storyset
+              </a>
+            </Group>
+          </span>
+        </Grid.Col>
+        <Grid.Col md={12} lg={6}>
+          <Group direction="column" align="center">
+            <h1>All events in one place.</h1>
+            <h1>Schedule events for teams.</h1>
+            <button
+              onClick={login}
+              className="bg-primary px-20 py-2 rounded-md cursor-pointer text-white"
+            >
+              Login
+            </button>
+          </Group>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 };
