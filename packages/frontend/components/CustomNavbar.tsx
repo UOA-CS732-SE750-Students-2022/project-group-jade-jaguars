22b9c86eb03681b3d26e6cf8cb7e21f8946025cd
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2022-05-02 22:15:57 +1200

    basic firebase auth on frontend and styling for landing page (#64)

--- packages/frontend/components/CustomNavbar.tsx
+++ packages/frontend/components/CustomNavbar.tsx
@@ -1,8 +1,12 @@
 import { Avatar, Group, Image, Navbar } from '@mantine/core';
 import { useViewportSize } from '@mantine/hooks';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CalendarEvent, LayoutDashboard, Users } from 'tabler-icons-react';
+import { useAuth } from '../src/context/AuthContext';
 import { NavbarLink } from './NavbarLink';
+import { getAuth } from 'firebase/auth';
+import { useRouter } from 'next/router';
+
 const linkData = [
   { icon: LayoutDashboard, label: 'Dashboard' },
   { icon: CalendarEvent, label: 'Events' },
@@ -10,15 +14,23 @@ const linkData = [
 ];
 export const CustomNavbar = () => {
   const { height, width } = useViewportSize();
-  const [active, setActive] = useState(2);
-  return (
-    <Navbar height={height} width={{ base: 100 }}>
+  const [active, setActive] = useState(0);
+  const { user, setUser } = useAuth();
+  const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    user ? setIsLogin(true) : setIsLogin(false);
+    !user && router.push('/');
+  }, [user]);
+
+  return isLogin ? (
+    <Navbar height={height} width={{ base: 100 }} className="border-1">
       <Navbar.Section>
-        <Group direction="column" align="center" mt={50}>
-          <Avatar color="teal" radius="lg" size={70}>
-            Logo
-          </Avatar>
-        </Group>
+        <div className="w-[100px]">
+          <Group direction="column" align="center" mt={10}>
+            <img src="/logo.svg" width={50} />
+          </Group>
+        </div>
       </Navbar.Section>
       <Navbar.Section grow mt={100}>
         <Group direction="column" align="center" spacing="xs">
@@ -35,5 +47,7 @@ export const CustomNavbar = () => {
         </Group>
       </Navbar.Section>
     </Navbar>
+  ) : (
+    <div></div>
   );
 };
