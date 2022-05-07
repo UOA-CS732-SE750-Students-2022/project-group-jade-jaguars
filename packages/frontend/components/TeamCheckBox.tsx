4327a2ff7b324a06e0ed7fca677c7758bf4d221d
Raina Song <rainasong0509@gmail.com>
2022-05-08 01:59:10 +1200

    Modify style for TeamCheckBox

--- packages/frontend/components/TeamCheckBox.tsx
+++ packages/frontend/components/TeamCheckBox.tsx
@@ -18,9 +18,9 @@ export const TeamCheckBox: React.FC<TeamCheckBoxProps> = ({
     '#FF9F9F',
     '#E2B1FF',
   ];
-  const color = 'teal';
+  const color = 'transparent';
   const inputBackgroundColor = inputBackgroundColorList[order % 5];
-  const inputStyle = `ml-3 border-solid border-black border-2 cursor-pointer`;
+  const inputStyle = `ml-3 border-solid border-black border-2 rounded-md cursor-pointer`;
   const { hovered, ref } = useHover();
   const [checked, setChecked] = useState(false);
   return (
@@ -35,7 +35,7 @@ export const TeamCheckBox: React.FC<TeamCheckBoxProps> = ({
         color={color}
         label={label}
         classNames={{
-          root: 'hover:bg-[#99C08B] w-full h-full rounded-md',
+          root: 'hover:bg-primary bg-cardgrey w-full h-full py-1 px-1 rounded-lg',
           label: hovered
             ? 'w-full p-2 ml-3 text-white cursor-pointer'
             : 'w-full p-2 ml-3 text-black cursor-pointer',
