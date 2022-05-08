ae29ae83d31ea54210c3a0a0c581780925ea03e7
Samuel Chen <luffymax0322@gmail.com>
2022-05-08 17:28:29 +1200

    skeleton of event final card

--- packages/frontend/pages/finalised/[id].tsx
+++ packages/frontend/pages/finalised/[id].tsx
@@ -1,4 +1,4 @@
-import { Paper, Grid, Box } from '@mantine/core';
+import { Paper, Grid, Box, Container, Group, Button } from '@mantine/core';
 import axios from 'axios';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
@@ -23,11 +23,40 @@ const FinalisedEventPageWithId: NextPage = () => {
     getEvent();
   }, []);
   return (
-    <Box>
-      <Paper>
-        <Grid children={undefined}></Grid>
-      </Paper>
-    </Box>
+    <Container>
+      <h1>Your event is finalised!</h1>
+      <Box sx={{ maxWidth: 700 }} mx="auto">
+        <Paper p="xl" radius="md" withBorder>
+          <Grid className="m-[50px] text-center">
+            <Grid.Col>
+              <h2 className="text-7xl">Event Title</h2>
+            </Grid.Col>
+            <Grid.Col>
+              <h3 className="text-bold text-2xl">2022.5.4 13:30-15:30 NZDT</h3>
+            </Grid.Col>
+            <Grid.Col>
+              <h4 className="italic text-md">the is a short description</h4>
+            </Grid.Col>
+            <Grid.Col>
+              <p className="mb-[-5px]">See you at</p>
+              <h3 className="text-xl">OGGB 260.115</h3>
+            </Grid.Col>
+            <Grid.Col>
+              <Group position="right" mt="lg">
+                <Button
+                  classNames={{
+                    filled: 'bg-[#FFDF74] hover:bg-[#FFDF74]',
+                    label: 'text-black',
+                  }}
+                >
+                  Add to calendar
+                </Button>
+              </Group>
+            </Grid.Col>
+          </Grid>
+        </Paper>
+      </Box>
+    </Container>
   );
 };
 export default FinalisedEventPageWithId;
