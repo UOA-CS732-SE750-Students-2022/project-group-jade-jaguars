e10db52c8f9078d0fa3476ed7819d277738dfb04
Raina Song <rainasong0509@gmail.com>
2022-05-08 20:13:09 +1200

    Clean up unused code

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -7,10 +7,7 @@ const Home: NextPage = () => {
   const { userId, authToken, user, login, logout, signedIn, setUser } =
     useAuth();
   const router = useRouter();
-  console.log(authToken);
   useEffect(() => {
-    localStorage.setItem('authToken', authToken);
-    localStorage.setItem('userId', userId);
     signedIn ? router.push('/') : router.push('/login');
   }, [signedIn]);
 
