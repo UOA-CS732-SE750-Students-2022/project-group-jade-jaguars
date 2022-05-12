0319a7e9b7836b32b4c9c981b247950b76b9c3cb
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2022-05-12 16:22:47 +1200

    store checked team in a string[] useState (#113)

--- packages/frontend/components/CheckBoxList.tsx
+++ packages/frontend/components/CheckBoxList.tsx
@@ -1,8 +1,9 @@
 import { Checkbox, ScrollArea } from '@mantine/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { TeamCheckBox } from './TeamCheckBox';
 
 const CheckBoxList = () => {
+  const [checkedTeamList, setCheckedTeamList] = useState<string[]>([]);
   const teamList = [
     '701',
     '750',
@@ -13,11 +14,28 @@ const CheckBoxList = () => {
     'Board Game Club',
     'Board Game Club',
   ];
+  const handleClick = (checked: boolean, label: string) => {
+    let temp;
+    if (checked) {
+      temp = [...checkedTeamList];
+      temp = temp.filter((name) => name != label);
+    } else {
+      temp = [...checkedTeamList, label];
+    }
+    setCheckedTeamList(temp);
+  };
 
   return (
     <ScrollArea style={{ height: 100, width: 300 }}>
       {teamList.map((team, index) => {
-        return <TeamCheckBox label={team} order={index} key={index} />;
+        return (
+          <TeamCheckBox
+            label={team}
+            order={index}
+            key={index}
+            handleClick={handleClick}
+          />
+        );
       })}
     </ScrollArea>
   );
