09580fb768ffbea940ee91e8ab78b35c36ca3606
Raina Song <rainasong0509@gmail.com>
2022-05-14 23:43:25 +1200

    Adjust EventCard style

--- packages/frontend/components/EventCard/EventCard.tsx
+++ packages/frontend/components/EventCard/EventCard.tsx
@@ -47,32 +47,36 @@ const EventCard = (props: EventCardInterface) => {
         id="header"
         className="flex flex-row items-center justify-between mb-1 font-medium"
       >
-        <h1 className="text-2.5xl flex-1 truncate ... mr-5">{title}</h1>
+        <p className="text-[25px] flex-1 truncate ... mr-5">{title}</p>
         <div>
-          <Avatar.Group
-            count={
-              participants.length - 4 > 0 ? participants.length - 4 : undefined
-            }
-            animated={false}
-          >
-            {participants.slice(0, 4).map((participant, index) => (
-              <Avatar
-                key={index}
-                size="md"
-                pointer
-                text={participant.name}
-                src={participant.profilePic}
-                stacked
-                bordered
-                borderWeight={'light'}
-              />
-            ))}
-          </Avatar.Group>
+          {participants && (
+            <Avatar.Group
+              count={
+                participants.length - 4 > 0
+                  ? participants.length - 4
+                  : undefined
+              }
+              animated={false}
+            >
+              {participants.slice(0, 4).map((participant, index) => (
+                <Avatar
+                  key={index}
+                  size="md"
+                  pointer
+                  text={participant.name}
+                  src={participant.profilePic}
+                  stacked
+                  bordered
+                  borderWeight={'light'}
+                />
+              ))}
+            </Avatar.Group>
+          )}
         </div>
       </div>
       <div id="date-time" className="text-lg font-normal">
         <p>
-          {date.toLocaleDateString()},{' '}
+          {date?.toLocaleDateString()},{' '}
           {`${timeRange[0].getHours()}:${timeRange[0].getMinutes()}`} -{' '}
           {`${timeRange[1].getHours()}:${timeRange[1].getMinutes()}`} NZDT
         </p>
