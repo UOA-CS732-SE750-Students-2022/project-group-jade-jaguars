a23e68895ccf8d13e69f1e325a64fc1c2714d38f
Samuel Chen <luffymax0322@gmail.com>
2022-05-15 14:01:18 +1200

    fix

--- packages/frontend/components/EventForm/EventForm.tsx
+++ packages/frontend/components/EventForm/EventForm.tsx
@@ -14,7 +14,7 @@ import {
 import { TimePicker } from 'antd';
 import moment from 'moment';
 import { UseForm } from '@mantine/hooks/lib/use-form/use-form';
-import Event from '../types/Event';
+import Event from '../../types/Event';
 interface FormValues {
   title: string;
   dateRange: [Date | null, Date | null];
