dd7e942a4f25220999d184082aaa165dd9cdc444
Raina Song <rainasong0509@gmail.com>
2022-05-17 20:11:57 +1200

    invitation page style issue fix

--- packages/frontend/pages/join.tsx
+++ packages/frontend/pages/join.tsx
@@ -29,35 +29,37 @@ const JoinPage: NextPage = () => {
 
   return (
     <>
-      <Container mt={200}>
-        <h1>Enter your invitation code</h1>
-        <Paper p="xl" radius="md" withBorder>
-          <Grid>
-            <Grid.Col>
-              <TextInput
-                value={form.values.code}
-                placeholder="enter your code"
-                onChange={(e) =>
-                  form.setFieldValue('code', e.currentTarget.value)
-                }
-              />
-            </Grid.Col>
-            <Grid.Col>
-              <Group position="right" mt="lg">
-                <Button
-                  classNames={{
-                    filled: 'bg-[#FFDF74] hover:bg-[#FFDF74]',
-                    label: 'text-black',
-                    root: 'hover:bg-[#ffeeb0]',
-                  }}
-                  onClick={onClick}
-                >
-                  <p className="font-medium">Submit</p>
-                </Button>
-              </Group>
-            </Grid.Col>
-          </Grid>
-        </Paper>
+      <Container style={{ maxWidth: '100vw' }} className="m-0 p-0">
+        <div className="min-w-[1200px] flex flex-col h-full gap-5 py-28 w-full items-center justify-center">
+          <h1>Enter your invitation code</h1>
+          <Paper style={{ width: '700px' }} p="xl" radius="md" withBorder>
+            <Grid>
+              <Grid.Col>
+                <TextInput
+                  value={form.values.code}
+                  placeholder="enter your code"
+                  onChange={(e) =>
+                    form.setFieldValue('code', e.currentTarget.value)
+                  }
+                />
+              </Grid.Col>
+              <Grid.Col>
+                <Group position="right" mt="lg">
+                  <Button
+                    classNames={{
+                      filled: 'bg-[#FFDF74] hover:bg-[#FFDF74]',
+                      label: 'text-black',
+                      root: 'hover:bg-[#ffeeb0]',
+                    }}
+                    onClick={onClick}
+                  >
+                    <p className="font-medium">Submit</p>
+                  </Button>
+                </Group>
+              </Grid.Col>
+            </Grid>
+          </Paper>
+        </div>
       </Container>
     </>
   );
